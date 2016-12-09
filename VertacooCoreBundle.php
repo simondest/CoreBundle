27409994ea4bcfd9778c6ac96a78fb015fca5304
Simon Destombes <simon@vercors-net.com>
2016-12-09 16:51:49 +0100

    add fieldset form type

--- VertacooCoreBundle.php
+++ VertacooCoreBundle.php
@@ -1,9 +1,16 @@
 <?php
 
 namespace Vertacoo\CoreBundle;
-
+use Vertacoo\CoreBundle\DependencyInjection\Compiler\FieldsetCompilerPass;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
 
 class VertacooCoreBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new FieldsetCompilerPass());
+    }
 }
